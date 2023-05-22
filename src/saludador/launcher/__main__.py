08a6e05c8aa110e2d7805abaf5f0ffc2aebd03b2
Jaime Feldman <jaime.feldman@mac.com>
2023-05-22 00:42:15 -0400

    Resolviendo el conficto en el main

--- src/saludador/launcher/__main__.py
+++ src/saludador/launcher/__main__.py
@@ -6,16 +6,10 @@ import saludador.modules.files as files
 from termcolor import colored
 
 def main():
-<<<<<<< HEAD
     print("[", colored("Iniciando app de pruebas", "green"), f"] ver: {__version__}")
     print("- modulo saludador:", saludador.saluda_a("jaime"))
     print(colored("* Abriendo archvio desde resoruces/texts/archivo.txt:", "light_grey"))
     files.read_file("resources/textos/archivo.txt")
      
-=======
-    print("[", colored(f"Iniciando app de pruebas", "green"), f"] ver :{__version__}")
-    saludador.saluda_a("jaime") 
->>>>>>> refs/remotes/origin/main
-
 if __name__ == "__main__":
     sys.exit(main())
